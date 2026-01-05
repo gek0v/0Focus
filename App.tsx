8467c011400385c486c86949c3fba14a88c5bf6d
gek0v <angelrojosanz06@gmail.com>
2026-01-06 00:19:40 +0100

    fix: center pomodoro toggle thumb

--- App.tsx
+++ App.tsx
@@ -427,7 +427,7 @@ const App: React.FC = () => {
                     </div>
                     <label className="relative inline-flex items-center cursor-pointer pointer-events-none">
                       <input type="checkbox" checked={isPomodoro} readOnly className="sr-only peer" />
-                      <div className="w-14 h-7 bg-slate-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary/30 dark:peer-focus:ring-primary/20 rounded-full peer dark:bg-[#233329] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
+                      <div className="w-11 h-6 bg-slate-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary/30 dark:peer-focus:ring-primary/20 rounded-full peer dark:bg-[#233329] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
                     </label>
                   </div>
                 </div>
